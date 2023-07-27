7aeb4d82d6c61d13a2854d79fd2c96724936b261
Gavin Palmer <gavin@herolfg.com>
2023-07-27 12:44:21 +0000

    add logging

--- bot.py
+++ bot.py
@@ -2,6 +2,19 @@ from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+from google.cloud import logging
+import google.cloud.logging  # Don't conflict with standard logging
+from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
+
+client = google.cloud.logging.Client()
+handler = CloudLoggingHandler(client)
+google.cloud.logging.handlers.setup_logging(handler)
+
+# Now you can use python's standard logging
+
+import logging
+logger = logging.getLogger("gavinbot")
+logger.setLevel(logging.INFO)  # Or whatever level you want
 
 load_dotenv()
 
@@ -20,6 +33,7 @@ output_channels = [
 
 @multicast_bot.event
 async def on_message(message):
+    logger.info(message.content)
     if message.channel.id == input_channel:
         for channel_id in output_channels:
             channel = multicast_bot.get_channel(channel_id)
